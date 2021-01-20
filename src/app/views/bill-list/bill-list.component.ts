44e1eb377c49f1e706599fbc98934b26dab0b9f8
Guilherme <guigo_gaara@hotmail.com>
2021-01-20 20:21:08 -0300

    fix: on cancel bug fixed

--- src/app/views/bill-list/bill-list.component.ts
+++ src/app/views/bill-list/bill-list.component.ts
@@ -1,28 +1,8 @@
 import { BillService } from 'src/app/shared/bill-service.service';
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 @Component({
   selector: 'app-bill-list',
@@ -30,26 +10,34 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./bill-list.component.css']
 })
 export class BillListComponent implements AfterViewInit  {
-  
+
   dataSource: MatTableDataSource<any>;
-  displayedColumns: string[] = ['name', 'price', 'createdAt','dueDate','actions'];
+  displayedColumns: string[] = ['status','name', 'price', 'createdAt','dueDate','actions'];
   
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(private billService: BillService) { }
-  ngAfterViewInit(): void {
-    
-    
+  ngAfterViewInit(): void {       
+    this.onLoad()
+  }
+
+  onLoad(){
     this.billService.listBills().subscribe((data:any) => {
       this.dataSource = new MatTableDataSource(data.results)
       this.dataSource.sort = this.sort
       this.dataSource.paginator = this.paginator
-      
     })
   }
+  onView(billId){
+    window.open(`https://sandbox.easypag.com.br/api/v1/invoices/${billId.id}/view/boleto`, "_blank");
+  }
 
-  onCancel(){
-    // this.billService.cancelBill('ad5b03ff-f9ed-41bb-b348-37687c374677')
+  onCancel(elem){
+    this.billService.cancelBill(elem.id).subscribe((data: Object) => {
+      console.log('succes', data)
+      this.onLoad()
+    })
+    this.onLoad()
   }
 }
